0e1f9e663f9b1238538f6b4df5c42f978b8294ba
fellpos <ra54399754893@acaonsfatima.org.br>
2024-07-19 14:17:07 -0300

    testev2

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h1>q saudade de fazer um corte insano na cabeça de alguem</h1>
+      <h1>instalando clonacartao.exe </h1>
     </div>
   );
 }
