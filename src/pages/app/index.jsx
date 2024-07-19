8636fd0a02112097cdc7f24de1711751cdf86111
fellpos <ra54399754893@acaonsfatima.org.br>
2024-07-19 16:44:49 -0300

    ultima vez

--- src/pages/app/index.jsx
+++ src/pages/app/index.jsx
@@ -3,8 +3,8 @@ import './index.css';
 export default function App() {
   return (
     <div className="App">
-      <h1>instalando clonacartao.exe
-      </h1>
+      <h1>instalando clonacartao.exe</h1>
+      <i className='fa fa-heart'></i>
     </div>
   );
 }
