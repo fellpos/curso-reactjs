f4a07ca96fe5375ab30d2e2fbbbe8a5c6a0aabe2
fellpos <ra54399754893@acaonsfatima.org.br>
2024-07-19 14:14:37 -0300

    teste

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h1>q saudade de t=fazer um corte insano na cabeça de alguem</h1>
+      <h1>q saudade de fazer um corte insano na cabeça de alguem</h1>
     </div>
   );
 }
